71582b6d0abc8de79e732ba0e3540d283986d0f2
HaBuDeSu <ruf.kenichi@gmail.com>
2019-09-20 11:40:46 -0500

    data fetched from axios and login page set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   const [colorList, setColorList] = useState([]);
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <Route exact path="/" render={props => <Login {...props} />} />
+        <Switch>
+          <PrivateRoute exact path="/bubble-page" component={BubblePage} />} />
+          <Route path="/login" render={props => <Login {...props} />} />
+        </Switch>
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = props => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const login = event => {
+    event.preventDefault();
+    setIsLoading(true);
+    axiosWithAuth()
+      .post("/api/login", credentials)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/bubble-page");
+      })
+      .catch(err => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  };
+
+  const onChange = event => {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <div className="login-page">
+        <form onSubmit={login}>
+          <label className="username-label">Username</label>
+          <input
+            type="text"
+            name="username"
+            onChange={onChange}
+            value={credentials.username}
+          />
+          <label className="password-label">Password</label>
+          <input
+            type="password"
+            name="password"
+            onChange={onChange}
+            value={credentials.password}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </>
   );
 };
